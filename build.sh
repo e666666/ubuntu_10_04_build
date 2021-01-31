8d57b9ea1a181efed808e3738b079e68e8c78425
Nyan cat <17372086+e666666@users.noreply.github.com>
2021-01-31 20:30:05 +0800

    No need to use && with set -e

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -e
-docker build -t x86_64-linux-glibc2.11-4.6 --ulimit nofile=1024 . && docker run --rm --entrypoint cat x86_64-linux-glibc2.11-4.6 /tmp/x86_64-linux-glibc2.11-4.6.tar.bz2 > x86_64-linux-glibc2.11-4.6-orig.tar.bz2
+docker build -t x86_64-linux-glibc2.11-4.6 --ulimit nofile=1024 .
+docker run --rm --entrypoint cat x86_64-linux-glibc2.11-4.6 /tmp/x86_64-linux-glibc2.11-4.6.tar.bz2 > x86_64-linux-glibc2.11-4.6-orig.tar.bz2
 scripts/postprocessing.sh
