169cf649e35e2f91c9d2f00ed971de28f3ac4303
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-01-31 20:17:59 +0800

    Change raw artifact name

--- scripts/postprocessing.sh
+++ scripts/postprocessing.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
 rm -rf x86_64-linux-glibc2.11-4.6
-tar xvf x86_64-linux-glibc2.11-4.6.tar.bz2
-cd x86_64-linux-glibc2.11-4.6
+tar xvf x86_64-linux-glibc2.11-4.6-orig.tar.bz2
+pushd x86_64-linux-glibc2.11-4.6
+popd
