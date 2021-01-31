056989d87d6691ee2ef8fcf0fd2a7b20e6bdace5
Nyan Cat <17372086+e666666@users.noreply.github.com>
2021-01-31 21:34:52 +0800

    Implement commit 1

--- scripts/postprocessing.sh
+++ scripts/postprocessing.sh
@@ -3,4 +3,11 @@ set -e
 rm -rf x86_64-linux-glibc2.11-4.6
 tar xvf x86_64-linux-glibc2.11-4.6-orig.tar.bz2
 pushd x86_64-linux-glibc2.11-4.6
-popd
+
+# Add missing libxcb.so.*
+docker run --rm --entrypoint cat x86_64-linux-glibc2.11-4.6 /usr/lib/libxcb.so.1.1.0 > sysroot/usr/lib/libxcb.so.1.1.0
+ln -sv libxcb.so.1.1.0 sysroot/usr/lib/libxcb.so.1
+docker run --rm --entrypoint cat x86_64-linux-glibc2.11-4.6 /usr/lib32/libxcb.so.1.1.0 > sysroot/usr/lib32/libxcb.so.1.1.0
+ln -sv libxcb.so.1.1.0 sysroot/usr/lib32/libxcb.so.1
+
+# Update headers
